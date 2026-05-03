4de0489934f4730845a2f5357ad295d0a4f9eeb6
Sharon Samuel Halli <sharonsamuelhalli@gmail.com>
2026-05-03 23:10:38 +0530

    fix: corrected models, database setup, tables created

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,5 +1,14 @@
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-db_url= "postresql://postgres:1234@localhost:5432/IntelliSight_db"
-engine= create_engine(db_url)
-SessionLocal= sessionmaker()
+from sqlalchemy.orm import sessionmaker
+
+db_url = "postgresql://postgres:1234@localhost:5432/IntelliSight_db"
+
+engine = create_engine(db_url)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
